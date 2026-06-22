007dab5de705e93a600301cccf8850b88b69bcf4
Shyddaas <shyddaas@gmail.com>
2026-06-22 19:41:43 +0530

    anylitics adding

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,15 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      {/* <!-- Google tag (gtag.js) --> */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-GH3TQSFC26"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-GH3TQSFC26');
+      </script>
       <body>
         <Header />
         {children}
